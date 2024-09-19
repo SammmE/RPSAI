92e72043744906a01896083af2d0f665d9bc8e67
sammme <samhithenganti@gmail.com>
2024-09-19 18:02:38 -0400

    added basic python project

---
+++ scripts/download_dataset.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+mkdir data
+
+# Download the dataset
+wget https://raw.githubusercontent.com/PizzaRollExpert/Rock-paper-scissors-data/refs/heads/master/data.txt -O data/initial_data.txt
